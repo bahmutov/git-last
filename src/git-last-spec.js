d15d7603abf07faefaa663c9a7295a493ede634b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-27 10:28:16 -0400

    initial code

---
+++ src/git-last-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const gitLast = require('.')
+
+describe('git-last', () => {
+  it('write this test', () => {
+    console.assert(gitLast, 'should export something')
+  })
+})
